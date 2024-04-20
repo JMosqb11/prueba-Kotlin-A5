eb53cf34efdc7edfbd4278e7cc8ad784a555e883
Jhonier Mosquera <jhoniermosquera61@gmail.com>
2024-04-20 15:10:15 -0500

    result

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    println("Hola Mundo")
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+
+    amanda.showProfile()
+    atiqah.showProfile()
 }
+
+class Person(private val name: String, private val age: Int,private val hobby: String?, private val referrer: Person?) {
+    fun showProfile() {
+        println("Name: ${name}\n" +
+                "Age: ${age}\n" +
+                "Likes to ${hobby}. ${if (referrer == null) "Doesn't have a referrer." else "Has a referrer named Amanda, who likes to play tennis."}")
+    }
+}
+
